dcbaa90091d3b481009f5885f6e7954c31e19f9a
Renier Morales <rmorales@octolabs.io>
2023-09-08 15:44:34 -0500

    fix luasnip event firing and add an autosave autocommand

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,14 +2,45 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds hereby
 
-local grp = vim.api.nvim_create_augroup("DynamicWinBar", { clear = true })
+-- Set a sensible winbar text for buffers except when the filetype is undetermined
+-- or when the buffer is neo-tree.
+local winbargrp = vim.api.nvim_create_augroup("DynamicWinBar", { clear = true })
 vim.api.nvim_create_autocmd("BufWinEnter", {
-  group = grp,
+  group = winbargrp,
   pattern = { "*.*", "*file" },
   callback = function()
     if vim.bo.filetype == "neotree" or vim.bo.filetype == "" then
       return
     end
     vim.wo.winbar = "%=%m %f"
+  end,
+})
+
+-- Always assume Edit Always if there is a swap file for the opened file.
+local swapgrp = vim.api.nvim_create_augroup("SwapChoice", { clear = true })
+vim.api.nvim_create_autocmd("SwapExists", {
+  group = swapgrp,
+  pattern = { "*" },
+  callback = function()
+    vim.swapchoice = "e"
+  end,
+})
+
+-- AutoSave buffers when leaving insert mode
+local savegrp = vim.api.nvim_create_augroup("AutoSave", { clear = true })
+vim.api.nvim_create_autocmd("InsertLeave", {
+  group = savegrp,
+  pattern = { "*" },
+  callback = function(args)
+    if vim.fn.getbufvar(args.buf, "&modifiable") ~= 1 then
+      return
+    end
+    if vim.bo.filetype == "" or vim.bo.filetype == "neotree" then
+      return
+    end
+    if not vim.api.nvim_buf_get_option(args.buf, "modified") then
+      return
+    end
+    vim.cmd("silent! write")
   end,
 })
---
+++ lua/plugins/luasnip.lua
@@ -0,0 +1,7 @@
+return {
+  "L3MON4D3/LuaSnip",
+  opts = {
+    delete_check_events = "TextChanged,InsertEnter",
+    region_check_events = "CursorHold,InsertLeave",
+  },
+}
