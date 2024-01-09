3d247800d2658d8ad5f69f20636fbc66bedf5df6
Renier Morales <rmorales@octolabs.io>
2024-01-09 12:34:46 -0600

    fix tab key issues with luasnip

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,12 +1,11 @@
 return {
   "L3MON4D3/LuaSnip",
   opts = {
-    delete_check_events = "TextChanged,InsertEnter",
-    region_check_events = "CursorHold,InsertLeave",
-    -- Uncomment if I keep getting issues with the tab character in insert mode jumping
-    -- to random locations.
-    keys = function()
-      return {}
-    end,
+    history = true,
+    region_check_events = "InsertEnter",
+    delete_check_events = "TextChanged",
   },
+  keys = function()
+    return {}
+  end,
 }
