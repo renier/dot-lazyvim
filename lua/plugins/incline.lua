50fa920abd650dea6e9c76c076f090480e18a09a
Renier Morales <rmorales@octolabs.io>
2024-04-11 17:36:16 -0500

    add incline, svelte, and toml plugins

---
+++ lua/plugins/incline.lua
@@ -0,0 +1,49 @@
+local devicons = require("nvim-web-devicons")
+local render = function(props)
+  local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+  if filename == "" then
+    filename = "[No Name]"
+  end
+  local ft_icon, ft_color = devicons.get_icon_color(filename)
+
+  local function get_git_diff()
+    local icons = { removed = " ", changed = " ", added = " " }
+    local signs = vim.b[props.buf].gitsigns_status_dict
+    local labels = {}
+    if signs == nil then
+      return labels
+    end
+    for name, icon in pairs(icons) do
+      if tonumber(signs[name]) and signs[name] > 0 then
+        table.insert(labels, { icon .. signs[name] .. " ", group = "Diff" .. name })
+      end
+    end
+    if #labels > 0 then
+      table.insert(labels, { "┊ " })
+    end
+    return labels
+  end
+
+  return {
+    { get_git_diff() },
+    { (ft_icon or "") .. " ", guifg = ft_color, guibg = "none" },
+    { filename .. " ", gui = vim.bo[props.buf].modified and "bold,italic" or "bold" },
+    -- { "┊  " .. vim.api.nvim_win_get_number(props.win), group = "DevIconWindows" },
+  }
+end
+
+return {
+  "b0o/incline.nvim",
+  config = function()
+    require("incline").setup({
+      render = render,
+      window = {
+        margin = {
+          horizontal = 0,
+          vertical = 0,
+        },
+      },
+    })
+  end,
+  event = "VeryLazy",
+}
