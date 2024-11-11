9c0afaa09ad1083445f72dc85a05566e77bcbf7d
Renier Morales <renier@morales-rodriguez.net>
2024-11-10 22:56:17 -0600

    sync

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -9,9 +9,17 @@ return {
         json = { "jq" },
         yaml = { "yamlfmt" },
         terraform = { "terraform_fmt" },
+        rust = { "rustfmt" },
+        proto = { "buf" },
       }
       opts.formatters.goimports = {
         prepend_args = { "-local", "code.8labs.io" },
+      }
+      opts.formatters.yamlfmt = {
+        prepend_args = { "-conf", "/Users/rmorales/.yamlfmt" },
+      }
+      opts.formatters.prettier = {
+        prepend_args = { "--cache" },
       }
       opts.default_format_opts = {
         timeout_ms = 750,
--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,10 +1,17 @@
 return {
   {
     "rcarriga/nvim-dap-ui",
-    dependencies = "mfussenegger/nvim-dap",
+    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
   },
   {
     "leoluz/nvim-dap-go",
     ft = "go",
+    opts = function(_, opts)
+      opts.delve = {
+        build_flags = "-tags=integration",
+        -- build_flags = "-ldflags=-linkmode=internal",
+      }
+      opts.tests = { verbose = true }
+    end,
   },
 }
