8df31a332764a82cb432aa54b00b41a920129fbe
Renier Morales <renier@morales-rodriguez.net>
2026-06-06 23:27:30 -0500

    theme and ai updates

--- lua/plugins/nightfox.lua
+++ lua/plugins/nightfox.lua
@@ -2,11 +2,44 @@ return {
   {
     "EdenEast/nightfox.nvim",
     lazy = false,
+    version = false,
+    priority = 1000,
     config = function()
       require("nightfox").setup({
         options = {
           transparent = true,
         },
+      })
+    end,
+  },
+  {
+    "savq/melange-nvim",
+    lazy = false,
+    version = false,
+    priority = 1000,
+  },
+  {
+    "sainnhe/sonokai",
+    lazy = false,
+    priority = 1000,
+    version = false,
+    config = function()
+      vim.g.sonokai_style = "shusia"
+      vim.g.sonokai_cursor = "auto"
+      vim.g.sonokai_enable_italic = true
+      vim.g.sonokai_transparent_background = false
+    end,
+  },
+  {
+    "neanias/everforest-nvim",
+    version = false,
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("everforest").setup({
+        background = "hard",
+        transparent_background_level = 0,
+        ui_contrast = "high",
       })
     end,
   },
